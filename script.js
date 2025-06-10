513639cb7ba1693b8bd426e535c1580e8dd483bd
rgerbasi <31828557+rgerbasi@users.noreply.github.com>
2025-06-10 19:16:30 +0000

    add: missing button ids, starter button logic

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // script.js
-console.log("hello world")
-// alert("hello!")
 
 /* 
-Current Idea: 
+Current Idea: get all buttons, switch statement for different listeners
 */
+
+const buttons = document.querySelectorAll('button');
+console.log(buttons)
+
+function print(thing) { console.log(thing);}
+
+buttons.forEach((button) => {
+    //assign proper listener
+    if (!button.id) return;
+    
+    if (!isNaN(button.id)) {
+        //number button listeners
+        // print(button.id)
+    } else {
+        switch(button.id) {
+            default:
+                print(button)
+        }
+    } // end of if button.id is number
+});
